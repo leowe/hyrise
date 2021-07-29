0d38cb62353231e98d5693add0e555b7aa20d0ff
leowe <leowndt@gmail.com>
2021-07-29 21:57:57 +0000

    fix script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-BUILD_DIR="build_release"
-OUTPUT_NAME_PRE="benchmark_wo_semijoin_reduction_k_"
+BUILD_DIR="build-release"
+OUTPUT_NAME_PRE="benchmark_as_generic_wo_semijoin_reduction_k_"
 OUTPUT_NAME_POST=".json"
 
 KS=(10 50 100 1000 5000)
 
-for K in KS
+for K in $KS
 do
 	echo "Running Benchmark For K = ${K}"
 	./${BUILD_DIR}/hyriseBenchmarkJoinOrder -r 100 -o "${OUTPUT_NAME_PRE}${K}${OUTPUT_NAME_POST}" --top_k ${K}
