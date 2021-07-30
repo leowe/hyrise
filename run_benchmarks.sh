b04e9f2b5752ed2f75aa70a70269c5785d9b1181
leowe <leowndt@gmail.com>
2021-07-30 09:51:21 +0000

    fix run script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -6,7 +6,7 @@ OUTPUT_NAME_POST=".json"
 
 KS=(10 50 100 1000 5000)
 
-for K in $KS
+for K in "${KS[@]}"
 do
 	echo "Running Benchmark For K = ${K}"
 	./${BUILD_DIR}/hyriseBenchmarkJoinOrder -r 100 -o "${OUTPUT_NAME_PRE}${K}${OUTPUT_NAME_POST}" --top_k ${K}
